5ec71e6841a580bca26f0139e62b40a9f3403881
Your Name <jonoreilly007@gmail.com>
2026-04-21 23:22:37 +0200

    feat: añadir FFT

--- filtros.jl
+++ filtros.jl
@@ -1,16 +1,16 @@
 using WAV
 using Plots
 
-# y, fs = wavread(raw".\aoe.wav")
-# y, fs = wavread(raw"C:\Windows\Media\Alarm02.wav")
-y, fs = wavread(raw"C:\Windows\Media\Ring01.wav")
-# y, fs = wavread(raw".\example.wav")
+y, frecuenciaMuestreo = wavread(raw".\aoe.wav")
+# y, frecuenciaMuestreo = wavread(raw"C:\Windows\Media\Alarm02.wav")
+# y, frecuenciaMuestreo = wavread(raw"C:\Windows\Media\Ring01.wav")
+# y, frecuenciaMuestreo = wavread(raw".\example.wav")
 
-function DFT(muestras::Vector{Float64}, frecuenciaMuestreo::Float32)
+function DFT(muestras::Vector{Float64}, frecuenciaMaxima::Float64)
 
     N = size(muestras)[1]
 
-    frecuencias = N / 10 + 1
+    frecuencias = Int(ceil(frecuenciaMaxima * N / frecuenciaMuestreo))
 
     componentesFrecuencias = (0:frecuencias-1) .|> f -> begin
 
@@ -20,9 +20,7 @@ function DFT(muestras::Vector{Float64}, frecuenciaMuestreo::Float32)
 
         imaginario = sum(muestras .* sin.(angulos))
 
-        frecuencia = f * frecuenciaMuestreo / N
-
-        return (real, imaginario, frecuencia)
+        return (real, imaginario)
 
     end
 
@@ -30,29 +28,145 @@ function DFT(muestras::Vector{Float64}, frecuenciaMuestreo::Float32)
 
 end
 
+function esPotencia2(n::Integer)
+    return n > 0 && (n & (n - 1)) == 0
+end
+
+function FFT(muestras::Vector{Tuple{Float64,Float64}})
+
+    N = size(muestras)[1]
+
+    if (N == 1)
+        return muestras
+    end
+
+    if (!esPotencia2(N))
+        siguientePotencia2 = 2^ceil(Int, log2(N))
+
+        muestrasPotencia2 = [muestras; (1:(siguientePotencia2-N)) .|> _ -> (0.0, 0.0)]
+
+        return FFT(muestrasPotencia2)
+    end
+
+    impar = muestras[1:2:end]
+
+    par = muestras[2:2:end]
+
+    fftImpar = FFT(impar)
+
+    fftPar = FFT(par)
+
+    angulos = (0:(N/2)-1) .* ((-2 * pi) / N)
+
+    senos = sin.(angulos)
+
+    cosenos = cos.(angulos)
+
+    fftParReal = fftPar .|> x -> x[1]
+
+    fftParImaginario = fftPar .|> x -> x[2]
+
+    fftImparReal = fftImpar .|> x -> x[1]
+
+    fftImparImaginario = fftImpar .|> x -> x[2]
+
+    twiddleFactorReal = (cosenos .* fftParReal) .+ (senos .* fftParImaginario)
+
+    twiddleFactorImaginario = (cosenos .* fftParImaginario) .- (senos .* fftParReal)
+
+    resltadoReal = [fftImparReal .+ twiddleFactorReal; fftImparReal .- twiddleFactorReal]
+
+    resltadoImaginario = [fftImparImaginario .+ twiddleFactorImaginario; fftImparImaginario .- twiddleFactorImaginario]
+
+    resultado = [(resltadoReal[i], resltadoImaginario[i]) for i in eachindex(resltadoReal)]
+
+    return resultado
+
+end
+
 function hann_window(bloque)
     N = size(bloque)[1]
 
-    return bloque .* [0.5 * (1 - cos(2π * n / (N - 1))) for n in 0:N-1]
+    return bloque .* [0.5 * (1 - cos(2π * n / (N - 1))) for n in (0:N-1)]
 end
 
-tamanioBloque = 600
+tamanioBloque = 2048
 
-bloquesMuestras = (1:Int(floor(tamanioBloque * 0.25)):((size(y)[1])-tamanioBloque)) .|> inicio -> hann_window(y[inicio:(inicio+tamanioBloque), 1])
+bloquesMuestras = [hann_window(y[inicio:(inicio+tamanioBloque), 1]) for inicio in (1:Int(floor(tamanioBloque * 0.25)):((size(y)[1])-tamanioBloque))]
+
+frecuenciaMaxima = 2_000.0
 
 println("typeof(bloquesMuestras): ", typeof(bloquesMuestras))
 
-dft = bloquesMuestras .|> x -> DFT([x; zeros(size(x)[1] * 10)], fs)
+# DFT
 
+println("DFT")
+
+tInicioDft = time()
+
+dft = DFT.(bloquesMuestras .|> x -> [x; zeros(size(x)[1])], frecuenciaMaxima)
+
+# dft = bloquesMuestras .|> x -> DFT([x; zeros(size(x)[1])])
+
+duracionDft = time() - tInicioDft
+
+println("duración DFT: ", round(duracionDft, digits=1), "s")
 println("typeof(dft): ", typeof(dft))
 println("size(dft): ", size(dft))
+println("size(dft[1]): ", size(dft[1]))
 
-intensidades = dft .|> b -> (b .|> f -> abs(f[1] + f[2] * im))
+intensidadesDft = dft .|> b -> (b .|> f -> abs(f[1] + f[2] * im))
 
-println("typeof(intensidades): ", typeof(intensidades))
-println("size(intensidades): ", size(intensidades))
+println("typeof(intensidadesDft): ", typeof(intensidadesDft))
+println("size(intensidadesDft): ", size(intensidadesDft))
+println("size(intensidadesDft[1]): ", size(intensidadesDft[1]))
 
-p = heatmap(1:size(intensidades)[1], 1:size(intensidades[1])[1], hcat(intensidades...), size=(1900, 900))
+contenedoresDeFrecuenciasDft = size(intensidadesDft[1])[1]
+
+ticksPorHzDft = contenedoresDeFrecuenciasDft / frecuenciaMaxima
+
+limiteEspectroDft = contenedoresDeFrecuenciasDft
+
+pasoDe100HzEnTicksDft = Int(floor(ticksPorHzDft * 100))
+
+yticksDft = (1:pasoDe100HzEnTicksDft:limiteEspectroDft, string.(round.(((0:pasoDe100HzEnTicksDft:limiteEspectroDft-1) .|> tick -> (tick / ticksPorHzDft) / 1000), digits=1)) .* " kHz")
+
+pDft = heatmap(1:size(intensidadesDft)[1], 1:limiteEspectroDft, hcat(intensidadesDft...)[1:limiteEspectroDft, :], size=(1900, 900), title="DFT ($(round(duracionDft, digits=1))s)", yticks=yticksDft)
+
+# FFT
+
+println("FFT")
+
+tInicioFft = time()
+
+fft = FFT.(bloquesMuestras .|> x -> (x .|> v -> (v, 0.0)))
+
+# fft = bloquesMuestras .|> x -> FFT([[(v, 0.0) for v in x]; ((1:size(x)[1]) .|> _ -> (0.0, 0.0))])
+
+duracionFft = time() - tInicioFft
+
+println("duración FFT: ", round(duracionFft, digits=1), "s")
+println("typeof(fft): ", typeof(fft))
+println("size(fft): ", size(fft))
+
+intensidadesFft = fft .|> b -> (b .|> f -> abs(f[1] + f[2] * im))
+
+println("typeof(intensidadesFft): ", typeof(intensidadesFft))
+println("size(intensidadesFft): ", size(intensidadesFft))
+
+contenedoresDeFrecuenciasFft = size(intensidadesFft[1])[1]
+
+ticksPorHzFft = contenedoresDeFrecuenciasFft / frecuenciaMuestreo
+
+limiteEspectroFft = Int(floor(ticksPorHzFft * frecuenciaMaxima + 1))
+
+pasoDe100HzEnTicksFft = Int(floor(ticksPorHzFft * 100))
+
+yticksFft = (1:pasoDe100HzEnTicksFft:limiteEspectroFft, string.(round.(((0:pasoDe100HzEnTicksFft:limiteEspectroFft-1) .|> tick -> (tick / ticksPorHzFft) / 1000), digits=1)) .* " kHz")
+
+pFft = heatmap(1:size(intensidadesFft)[1], 1:limiteEspectroFft, hcat(intensidadesFft...)[1:limiteEspectroFft, :], size=(1900, 900), title="FFT ($(round(duracionFft, digits=1))s)", yticks=yticksFft)
+
+p = plot(pDft, pFft, layout=(1, 2))
 
 display(p)
 
